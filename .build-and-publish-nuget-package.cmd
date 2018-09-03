81c955c1da1c9cd3d34e2af53283fa57ff19bfa4
avk <Andrew.Kostousov@gmail.com>
2018-09-03 13:02:27 +0500

    update build scripts

--- .build-and-publish-nuget-package.cmd
+++
@@ -1,5 +0,0 @@
-dotnet build --force --no-incremental --configuration Release ./GrobExp.Compiler.sln
-dotnet pack --no-build --configuration Release ./GrobExp.Compiler.sln
-cd ./GrobExp.Compiler/bin/Release
-dotnet nuget push *.nupkg -s https://nuget.org
-pause
