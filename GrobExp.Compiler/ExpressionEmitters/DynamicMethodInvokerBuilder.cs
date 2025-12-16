394283481f26ea84c3c56b2ef4f9904888171343
Sergei Lugovykh <s.lugovykh@skbkontur.ru>
2025-12-16 16:38:06 +0500

    use ThreadLocal Modules in LambdaCompiler to prevent intermittent segfaults

--- GrobExp.Compiler/ExpressionEmitters/DynamicMethodInvokerBuilder.cs
+++ GrobExp.Compiler/ExpressionEmitters/DynamicMethodInvokerBuilder.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
@@ -22,7 +21,7 @@ namespace GrobExp.Compiler.ExpressionEmitters
                 type = MonoSucks.Get(constantTypes, resultType, parameterTypes);
             else
             {
-                module = module ?? LambdaCompiler.Module;
+                module = module ?? LambdaCompiler.Module.Value;
                 var key = GetKey(module, constantTypes, resultType, parameterTypes);
                 type = (Type)types[key];
                 if (type == null)
@@ -170,8 +169,6 @@ namespace GrobExp.Compiler.ExpressionEmitters
         }
 
         public static readonly Func<DynamicMethod, IntPtr> DynamicMethodPointerExtractor = EmitDynamicMethodPointerExtractor();
-
-        private static readonly MethodInfo gcKeepAliveMethod = ((MethodCallExpression)((Expression<Action>)(() => GC.KeepAlive(null))).Body).Method;
 
         private static readonly Hashtable types = new Hashtable();
         private static readonly object typesLock = new object();
